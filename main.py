ffcc97a6dd31f22d45ff3014ad67e471e160fd07
PrateekKumar2109 <101380064+PrateekKumar2109@users.noreply.github.com>
2023-02-27 16:45:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ Follow Up Input: {question}
 Standalone question:"""
 CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
 
-prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+prompt_template = """You are an AI assistant for answering questions about the dilling activites of an oil and gas field. You are given the following extracted parts of a long document and a question. Provide a conversational answer. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 {context}
 Question: {question}
 Helpful Answer:"""
